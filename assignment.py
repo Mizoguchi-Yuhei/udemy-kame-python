2355052ef01d1e75bb8a3ab774d461de9d6d1e31
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-06-25 23:45:08 +0900

    3-13まで

--- assignment.py
+++ assignment.py
@@ -2,14 +2,21 @@
 hello = "こんにちは"
 world = "世界"
 print(hello + world)
-print("こんにちは" + "世界") # ハードコーディング
+print("こんにちは" + "世界")  # ハードコーディング
 
 # format
 "hello {}".format("world")
 print("{} {}".format(hello, world))
 
 name = "Emily"
-print("Hey {}!! How are you doing?".format(name))
+print("Hey, {}!! How are you doing?".format(name))
+print(f"Hey, {name}!! How are you doing?")
+
 
 balance = 100
 print("balance: {}".format(balance))
+print(f"balance: {balance}")
+
+
+# fstring
+print(f"{hello} {world}")
