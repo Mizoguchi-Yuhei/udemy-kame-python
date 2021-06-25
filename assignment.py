aa7294d5007e4e7b8ce5113efc568f343d3cd30b
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-06-25 23:17:53 +0900

    first commit

---
+++ assignment.py
@@ -0,0 +1,15 @@
+# 変数へ代入：assignment
+hello = "こんにちは"
+world = "世界"
+print(hello + world)
+print("こんにちは" + "世界") # ハードコーディング
+
+# format
+"hello {}".format("world")
+print("{} {}".format(hello, world))
+
+name = "Emily"
+print("Hey {}!! How are you doing?".format(name))
+
+balance = 100
+print("balance: {}".format(balance))
---
+++ string_basic.py
@@ -0,0 +1,20 @@
+# 文字列(String)
+
+print("Hello World!!")
+print('1')
+
+print("I'm fine.")
+print('I"m fine.')
+
+print("""
+Hello world!!
+How are you doing?
+""")
+
+print("Hello \nworld")
+print("Hello \tworld")
+
+print("back slash n: \\n")
+print('I\'m fine')
+
+print("hello" + "world" + "!!")
