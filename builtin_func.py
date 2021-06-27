b2a2406e6b1a30d780a3fff8668fa7208d372264
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-06-27 23:44:20 +0900

    3-17

---
+++ builtin_func.py
@@ -0,0 +1,14 @@
+# built in function
+# type()
+# hello_type = type("Hello")
+# print(hello_type)
+print(type(1))
+# id()
+hello_id = id("hello")
+print(hello_id)
+hello ="hello"
+hello2 = "hello"
+print(id(hello))
+w = "world"
+print(id(w))
+print(id("world"))
---
+++ error.py
@@ -0,0 +1,9 @@
+# result = 100 / 0  # バグ "bug" <- デバッグ "debug"
+# pritn("hello world!!")
+
+# result = "1" + 3
+result = int("1") + 3
+# print(result)
+
+# undefined_variable = "something"
+print(undefined_variable)
