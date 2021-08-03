62c3d9d720c375951cefee516f4082db4a9623c1
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-03 22:24:53 +0900

    3-35

---
+++ enumerate.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'peach', 'grapes', 'banana']
+
+for fruit in fruits:
+    print(fruit)
+
+for idx, fruit in enumerate(fruits):
+    # idx += 1
+    print(idx)
+    print(fruit)
+
+for x in enumerate(fruits):
+    print(x)
