f8fd38b1314926c20d6ea75e3777502a4372828e
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-09 20:23:33 +0900

    4-4-49

---
+++ function/type_annotation.py
@@ -0,0 +1,13 @@
+# Type annotation
+# def add_nums(num1: int, num2: int) -> int:
+def add_nums(num1, num2):
+    return num1 + num2
+
+
+# print(add_nums("1", "2"))
+
+# Pythonは動的型付け言語 <-> 静的型付け言語
+# one = 1
+# two = 2
+# one = 'hello'
+print(add_nums(1, 2))
