453731d0028b4177bd7fd3e59cb0852a3c28ad63
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-10 20:20:15 +0900

    4-53

---
+++ function/nested_function.py
@@ -0,0 +1,20 @@
+# 関数の中に関数を定義 (nested function)
+msg = "I am global."
+
+
+def outer():
+    msg = "I am outer."
+
+    def inner():
+        nonlocal msg
+        msg = "I am inner."
+        print("This is inner function.")
+        print(msg)
+    inner()
+    print(msg)
+
+
+outer()
+print(msg)
+
+# outer()
